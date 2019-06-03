e8a0bbb52107063a043e0f47aa2c0fddd76cca74
Oyekan Gbare <oyekanorisunmigbare@gmail.com>
2019-06-03 23:22:30 +0100

    fix: fixed wrong port issues

--- app/server.js
+++ app/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const routes = require('./routes')
 const app = express();
 const path = require('path')
-
+const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname,'..' ,'public')));
 
 app.use('/',routes)
+app.set('port', port);
 app.listen(3000, ()=>{
-    console.log('Server started at port 3000')
+    console.log(`Server started at port ${port}`)
 })

8532da23f24139fc35d587276a55eb7c2c0bb653
Orsunmigbare <oyekanoridunmigbare@gmail.com>
2019-08-28 05:04:47 -0700

    complete handlebars setup

---
+++ app/routes/blog.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Router = express.Router();
+
+Router.get('/', (req,res,next)=>{
+    res.render('home', {layout: 'default'})
+})
+Router.get('/category', (req,res,next)=>{
+    res.render('category', {layout: 'default'})
+})
+Router.get('/contact', (req,res,next)=>{
+    res.render('contact', {layout: 'default'})
+})
+Router.get('/about', (req,res,next)=>{
+    res.render('about', {layout: 'default'})
+})
+Router.get('/article', (req,res,next)=>{
+    res.render('article', {layout: 'default'})
+})
+
+module.exports = Router;
--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 const main_route = require('./routes/main_route');
 const api_route = require('./routes/api')
 const body_parser = require('body-parser')
+const blog_route = require('./routes/blog')
 const cors = require('cors');
 // const cookie_parser = require('cookie-parser');
 const app = express();
@@ -9,6 +11,14 @@ const path = require('path');
 const db = require('./db');
 const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, '..', 'public')));
+// configure hbs
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials'
+}))
 /* connect to db */
 db();
 
@@ -22,9 +32,9 @@ app.use(body_parser.urlencoded({
 
  
 // routes
-app.use('/', main_route)
+// app.use('/', main_route)
+app.use('/', blog_route)
 app.use('/api', api_route)
-
 
 
 app.set('port', port);
